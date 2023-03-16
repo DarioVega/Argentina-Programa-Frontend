0cd47a570d780c0908b7a95358de231435be5800
Darío Vega <sergiodariovega@gmail.com>
2023-03-16 02:18:59 -0300

    Quité el enrutador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { ServicesComponent } from './services/services.component';
-import { ContactComponent } from './contact/contact.component';
+// import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const appRoutes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'about',component:AboutComponent},
-  {path:'services',component:ServicesComponent},
-  {path:'contact',component:ContactComponent}
-];
+// const appRoutes: Routes = [
+//   {path:'',component:HomeComponent},
+//   {path:'about',component:AboutComponent},
+//   {path:'services',component:ServicesComponent},
+//   {path:'contact',component:ContactComponent}
+// ];
 
 @NgModule({
   declarations: [
@@ -25,7 +25,7 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    // RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
